e07ba6431e2aa95c639693ec8b27161a14ef1bb4
DecoCrafttech <decocrafttech@gmail.com>
2024-09-19 16:43:34 +0530

    UPDATE: home page services

--- src/components/home-v1.js
+++ src/components/home-v1.js
@@ -4,13 +4,14 @@ import Aboutv1 from './section-components/about-v1';
 import Counter from './section-components/counter-v1'; 
 import Testimonial from './section-components/testimonial-v1';
 import WhyChooseUs from './section-components/why-choose-us';
-
+import ServiceV5 from './service-v1';
 
 const Home_V1 = () => {
 
     return <>
         <Banner />
         <Aboutv1 />
+        <ServiceV5/>
         <Counter />
         <WhyChooseUs />
         <Testimonial /> 
